d1cd96d3f7cdac6a35ff55d038df6662001268ea
Yilong Sun <yilong-sun@lxuser-HP-ProBook-450-15-6-inch-G9-Notebook-PC>
2025-10-13 15:28:03 +0200

    Finish first project and start the second.

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { EXAMPLES } from "../data.js";
-
+import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
 import TabButton from "./TabButton.jsx";
 
 export default function Examples() {
@@ -24,15 +25,15 @@ export default function Examples() {
         )
     }
     return (
-        <section id="examples">
-            <h2>Examples</h2>
+        <Section title="Examples" id="examples">
+            <Tabs buttons={<><TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+                <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
+                <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
+                <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton></>}>{tabContent}</Tabs>
             <menu>
-                <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-                <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-                <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-                <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+
             </menu>
-            {tabContent}
+
             {/* 
           {!selectedTopic ? (<p>Please select a topic</p>) : (
             <div id="tab-content">
@@ -46,6 +47,6 @@ export default function Examples() {
             </div>)}
           */}
 
-        </section>
+        </Section>
     );
 }
